d99e3795cd5f7e4a5d0df7fd4070e2bd9a0bd236
BenB33 <benharrison9898@gmail.com>
2021-04-06 19:09:56 +0100

    3D Cube

--- Resources/Shaders/shader.vert.glsl
+++ Resources/Shaders/shader.vert.glsl
@@ -8,6 +8,6 @@ uniform mat4 u_model_matrix;
 uniform mat4 u_view_projection_matrix;
 
 void main() {
-	v_frag_pos = a_pos + 0.6;
+	v_frag_pos = a_pos;
 	gl_Position = u_view_projection_matrix * u_model_matrix * vec4(a_pos.x, a_pos.y, a_pos.z, 1.0);
 }
--- Source/cubedcrafter/main.cpp
+++ Source/cubedcrafter/main.cpp
@@ -14,21 +14,57 @@ int main()
 
 	window.setClearColour(0.1f, 0.4f, 0.6f, 1.0f);
 
-	//glEnable(GL_CULL_FACE);
-	//glCullFace(GL_BACK);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glEnable(GL_DEPTH_TEST);
 
 	float vertices[]
-	{  // x       y      z
-		-0.5f,  -0.5f,  0.0f,  // Bottom Left  (0)
-		 0.5f,  -0.5f,  0.0f, // Bottom Right  (1)
-		 0.5f,   0.5f,  0.0f, // Top Right     (2)
-		-0.5f,   0.5f,  0.0f, // Top Left	   (3)
+	{// x   y   z
+	    0,  0,  1,  // South Face +Z   (0)
+		1,  0,  1,  // 
+		1,  1,  1,  // 
+		0,  1,  1,  // 
+
+		1,  0,  0, // North Face -Z  (4)
+		0,  0,  0, //
+		0,  1,  0, //
+		1,  1,  0, //
+
+		0,  1,  0, // East Face +X (8)
+		0,  0,  0, // 
+		0,  0,  1, // 
+		0,  1,  1, // 
+		
+		1,  0,  0, // West Face -X (12)
+		1,  1,  0, //
+		1,  1,  1, //
+		1,  0,  1, //
+
+		1,  1,  0, // Top Face -Y (16)
+		0,  1,  0, // 
+		0,  1,  1, // 
+		1,  1,  1, // 
+
+	    0,  0,  0, // Bottom Face +Y (20)
+		1,  0,  0, // 
+		1,  0,  1, // 
+		0,  0,  1, // 
 	};
 
 	unsigned int indices[] =
 	{
-		0, 1, 2,
-		2, 3, 0,
+		0, 1,  2,
+		0, 2,  3,
+		4, 5,  6, 
+		4, 6,  7,
+		8, 9, 10, 
+		8, 10, 11,
+		12, 13, 14,
+		12, 14, 15,
+		16, 17, 18,
+		16, 18, 19,
+		20, 21, 22,
+		20, 22, 23,
 	};
 
 	Shader shader("../Resources/Shaders/shader");
